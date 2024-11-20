c25728649b9b4272286daed916c55323a331a78b
eito-ing <murata@in-g.jp>
2024-11-20 17:13:30 +0900

    お問合せ完了通知画面のUI

--- resources/views/contact/perfect.blade.php
+++ resources/views/contact/perfect.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container d-flex flex-column justify-content-center align-items-center" style="min-height: 40vh;">
-    <h2 class="text-success mb-4">お問い合わせありがとうございます！</h2>
-    <p class="text-muted text-center">お問い合わせ内容は正常に送信されました。<br>確認次第、折り返しご連絡いたします。</p>
-</div>
+    <div class="mt-5 d-flex flex-column justify-content-center align-items-center" >
+        <h2 class="text-success mb-4">お問い合わせありがとうございます！</h2>
+        <p class="text-muted text-center">お問い合わせ内容は正常に送信されました。<br>確認次第、折り返しご連絡いたします。</p>
+    </div>
 @endsection
